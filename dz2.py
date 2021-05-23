c5f744ab468906b40e86fb455a686419bb2e4dfb
Hanuma187 <casimhromoj@gmail.com>
2021-05-23 21:30:00 +0300

    Практическое задание ко 2му уроку

--- dz2.py
+++ dz2.py
@@ -40,11 +40,14 @@ print(list_x)
 for num in range(100):
     val=int(input("Введите следующий член рейштинга: "))
     max_val = max(list_x)
+    min_val = min(list_x)
     if val > max_val:
         list_x.insert(0, val)
-    elif val in list_x:
-        list_x.insert(list_x.index(val), val)
-    else:
+    elif val <= min_val:
         list_x.append(val)
+    else:
+        u=0
+        while (list_x[u] >= val) and (list_x[u+1] >= val):
+            u = u + 1
+        list_x.insert(u+1,val)
     print(list_x)
-
